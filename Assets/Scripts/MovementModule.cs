a4fd79dd5bb6f6bfdcadfefd34fa5bd372848a6b
Codey Huntting <creativecodey@gmail.com>
2021-01-18 13:34:54 -0800

    First Commit

---
+++ Assets/Scripts/MovementModule.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementModule : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The rigidbody to move")]
+    private Rigidbody2D rb2D;
+    [SerializeField]
+    [Tooltip("Power of the racer's acceleration")]
+    private float thrust;
+    [SerializeField]
+    [Tooltip("Tightness of the racer's turn")]
+    private float turn;
+
+    public MovementModule(Rigidbody2D rb2D, float thrust, float turn)
+    {
+        this.rb2D = rb2D;
+        this.thrust = thrust;
+        this.turn = turn;
+    }
+
+    public void Turn(float horizontal)
+    {
+        rb2D.MoveRotation(horizontal * turn * Time.fixedDeltaTime);
+    }
+
+    public void Thrust(float vertical)
+    {
+        rb2D.AddRelativeForce(new Vector2(0, vertical * thrust) * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Information used to move the cart")]
+    private MovementModule movementModule;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movementModule.Turn(horizontal);
+        movementModule.Thrust(vertical);
+    }
+}
