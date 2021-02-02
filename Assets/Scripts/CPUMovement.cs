c4034d9c1f0b0f25f666afeb9a2f4048ff68bd85
Codey Huntting <creativecodey@gmail.com>
2021-02-02 14:54:18 -0800

    Added some assets that can help us create a CPU drivers (may not work out in the end)

---
+++ Assets/Scripts/CPUMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CPUMovement : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent nav;
+    [SerializeField]
+    private Transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav.SetDestination(destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
