2f92f9bc2c328a4227934c3bd627a03aedd3387e
Codey Huntting <creativecodey@gmail.com>
2021-06-10 13:16:51 -0700

    Got boost audio to work and added new assets to the submodule

--- Assets/Scripts/Driving/BoostAudio.cs
+++ Assets/Scripts/Driving/BoostAudio.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using DG.Tweening;
+
 [System.Serializable]
 public class BoostAudio
 {
@@ -16,14 +18,21 @@ public class BoostAudio
     [Tooltip("Min-max pitch range for the audio clip")]
     private FloatRange pitchRange = new FloatRange(1f, 1.5f);
     [SerializeField]
-    [Tooltip("Minumum volume level of the underscore audio")]
-    private float minUnderscoreVolume = 0.1f;
+    [Tooltip("Time it takes for the audio to fade in when started")]
+    private float fadeIn = 0.2f;
     [SerializeField]
-    [Tooltip("Time it takes for the audio to fade out when finished")]
-    private float fadeTime = 1f;
+    [Tooltip("Time it takes for the audio to fade out when finsihed")]
+    private float fadeOut = 1f;
 
     public void StartAudio()
     {
+        // Complete any tweens (in case the fade-out is still running)
+        audio.DOComplete(true);
+
+        // Fade the audio in
+        audio.volume = 0f;
+        audio.DOFade(1f, fadeIn);
+
         // Play the audio that underscores the boost
         audio.clip = clip;
         audio.Play();
@@ -31,16 +40,17 @@ public class BoostAudio
 
     public void FixedUpdate(float boostLevel)
     {
-        // Change volume of the underscore as the boost increases/decreases level
-        //audio.volume = Mathf.Lerp(minUnderscoreVolume, 1f, boostLevel);
         // Change pitch based on current boost level
         audio.pitch = Mathf.LerpUnclamped(pitchRange.min, pitchRange.max, boostLevel);
     }
 
     public void StopAudio()
     {
-        // We need this to fade out tho, but we can't do it since this is not a MonoBehaviour...
-        // Time to check out the "DOTween" package!
-        audio.Stop();
+        // Complete any tweens (in case the fade-in is still running)
+        audio.DOComplete(true);
+
+        // Fade the audio out
+        audio.volume = 1f;
+        audio.DOFade(0f, fadeOut).OnComplete(() => audio.Stop());
     }
 }
