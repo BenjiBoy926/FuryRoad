80a2ef026fcc51cc3f4c99280ec9e97ba989530d
Codey Huntting <creativecodey@gmail.com>
2021-10-19 17:25:15 -0700

    First commit

---
+++ Assets/Scripts/Driving/Projectile.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    #region Private Editor Fields
+    [SerializeField]
+    [Tooltip("Reference to the rigidbody of the projectile")]
+    private Rigidbody rb;
+    [SerializeField]
+    [Tooltip("Reference to the root object of the projectile")]
+    private GameObject root;
+    [SerializeField]
+    [Tooltip("Transform of the object that renders a trail")]
+    private Transform trail;
+    #endregion
+
+    #region Private Fields
+    // Reference to the player who fired this projectile
+    private PlayerManager owner;
+    #endregion
+
+    #region Monobehaviour Messages
+    private void Update()
+    {
+        trail.position = transform.position;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Try to get a player manager in the parent of the object hit
+        PlayerManager other = collision.gameObject.GetComponentInParent<PlayerManager>();
+
+        if(other)
+        {
+            if (other == owner)
+            {
+                // Slow down the owner a little
+            }
+            // If the other is not the owner, then make the owner boost
+            else owner.movementDriver.movementModule.TryStartBoost();
+
+            // Destroy self when I hit a player
+            Destroy(root);
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public void Setup(PlayerManager owner, Vector3 velocity)
+    {
+        this.owner = owner;
+        rb.velocity = velocity;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Driving/ProjectileModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ProjectileModule
+{
+    #region Private Editor Fields
+    [SerializeField]
+    [Tooltip("Projectile prefab to instantiate when firing a projectile")]
+    private Projectile projectilePrefab;
+    #endregion
+
+    #region Public Methods
+
+    #endregion
+}
