08d5cb58108fed8681f9cdad40da13397332eced
Codey Huntting <creativecodey@gmail.com>
2021-10-28 14:44:44 -0700

    Basics of minimap
    Got the basics of minimap working for the player

---
+++ Assets/Scripts/Driving/MinimapController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : DrivingModule
+{
+    #region Private Editor Fields
+    [SerializeField]
+    [Tooltip("Reference to the transform of the camera that renders the minimap")]
+    private Transform minimapCameraRoot;
+    #endregion
+
+    #region Private Fields
+    // Offset of the camera, set at the start of the behaviour
+    private Vector3 offset;
+    #endregion
+
+    #region Monobehaviour Messages
+    protected override void Start()
+    {
+        base.Start();
+
+        // Set the camera's offset from the driving rigidbody
+        offset = minimapCameraRoot.position - manager.rigidbody.position;
+    }
+    private void Update()
+    {
+        // Set the position of the minimap camera on each update
+        minimapCameraRoot.position = manager.rigidbody.position + offset;
+
+        // Set the up of the camera to point in the direction the vehicle is facing
+        minimapCameraRoot.forward = manager.heading;
+    }
+    #endregion
+}
