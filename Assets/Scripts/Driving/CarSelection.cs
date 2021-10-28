4e732c73323e991062429bfba7ebad5554aad15b
christianbond <cbondfla@uci.edu>
2021-10-27 17:29:33 -0700

    Car Selection Scene Bones

---
+++ Assets/Scripts/Driving/CarSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSelection : MonoBehaviour
+{
+    private int currentCar;
+    [SerializeField] private Button previousButton;
+    [SerializeField] private Button nextButton;
+
+    private void Awake()
+    {
+        selectCar(0);
+    }
+
+    private void selectCar(int index)
+    {
+        previousButton.interactable = (index != 0);
+        nextButton.interactable = (index != transform.childCount-1);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(i == index);
+        }
+    }
+
+    public void ChangeCar(int change)
+    {
+        currentCar += change;
+        selectCar(currentCar);
+    }
+}
---
+++ Assets/Scripts/Driving/CarSelectionAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectionAnimation : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    [SerializeField] private Vector3 finalPosition;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, finalPosition, 0.1f);
+    }
+
+    private void OnDisable()
+    {
+        transform.position = initialPosition;
+    }
+}
