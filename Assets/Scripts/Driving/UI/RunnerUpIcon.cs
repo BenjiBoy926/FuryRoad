e9abdd81782d7c4777e079a29511b0b6bca7d6a0
Codey Huntting <creativecodey@gmail.com>
2022-01-30 19:24:31 -0800

    Got somewhere with the runner up ui
    It's extremely close to working properly, there's just this one big difficulty with the math that I cannot figure out yet. It's surprisingly hard to force the icon for the runner up to stay on the edge of the screen instead of hovering around inside the screen

---
+++ Assets/Scripts/Driving/UI/RunnerUpIcon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RunnerUpIcon : MonoBehaviour
+{
+    #region Public Properties
+    public RectTransform RectTransform => rectTransform;
+    #endregion
+
+    #region Private Editor Fields
+    [SerializeField]
+    [Tooltip("Reference to the rect transform used to place the icon")]
+    private RectTransform rectTransform;
+    [SerializeField]
+    [Tooltip("Text used to display which driver this icon represents")]
+    private TextMeshProUGUI text;
+    #endregion
+
+    #region Public Methods
+    public void DisplayDriver(DrivingManager driver)
+    {
+        // Try to get a network player on the driver
+        NetworkPlayer player = driver.GetComponent<NetworkPlayer>();
+
+        // If the player exists then display the network actor number
+        if (player) text.text = $"P{player.photonView.OwnerActorNr}";
+        // If the player does not exist use a placeholder
+        else text.text = "P0";
+    }
+    #endregion
+}
